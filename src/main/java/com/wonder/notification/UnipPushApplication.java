f7722937627fcb7ac8e15bba4361115f0e1a346b
Janindu-wse <janindu.spil@gmail.com>
2024-05-08 17:48:05 +0530

    init commit

---
+++ src/main/java/com/wonder/notification/UnipPushApplication.java
@@ -0,0 +1,13 @@
+package com.wonder.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnipPushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnipPushApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wonder/notification/UnipPushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wonder.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnipPushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
